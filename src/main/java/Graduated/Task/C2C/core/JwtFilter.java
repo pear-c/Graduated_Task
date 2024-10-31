5502510a0e2b6fe6d6d1fc38c9d91299bd62148a
pear_c <zxcv9676@naver.com>
2024-10-31 20:02:36 +0900

    10/31

--- src/main/java/Graduated/Task/C2C/core/JwtFilter.java
+++ src/main/java/Graduated/Task/C2C/core/JwtFilter.java
@@ -53,7 +53,6 @@ public class JwtFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
-
         filterChain.doFilter(request,response);
     }
 
